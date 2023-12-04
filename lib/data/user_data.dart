1c8ce1eeac6c01a2e750d19062a46c2ce4a35949
xamroyevxondamir68@gmail.com <xamroyevxondamir68@gmail.com>
2023-12-05 00:51:40 +0500

    Xondamir, 1-commit, User and Admin choose page, Login and Register choose page, Admin page (userInfo, userDelete, userBlock functions), User login page

--- lib/data/user_data.dart
+++ lib/data/user_data.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:console_words_app/services/extension_colors_service.dart';
+
 
 List<User> userListFromJson(String data)
 => List<User>.from(jsonDecode(data).map((e)
@@ -11,18 +11,13 @@ String userToJson(User user)
 class User{
 
   late String id;
-  late String name;
   late String username;
-  late int age;
   late String email;
   late String password;
-  late DateTime registerData;
-
+  late int registerData;
+  late bool isBlock;
   User({
-    required this.id,
-    required this.name,
     required this.username,
-    required this.age,
     required this.email,
     required this.password,
     required this.registerData,
@@ -30,23 +25,21 @@ class User{
 
   User.fromJson(Map<String, dynamic> json){
     id = json["id"];
-    name = json["name"];
     username = json["username"];
-    age = json["age"];
     email = json["email"];
     password = json["password"];
     registerData = json["registerData"];
+    isBlock = json["isBlock"];
   }
 
   Map<String, dynamic> toJson(){
     Map<String, dynamic> map = {
       "id": id,
-      "name": name,
       "username": username,
-      "age": age,
       "email": email,
       "password": password,
-      "registerData": registerData
+      "registerData": registerData,
+      "isBlock": isBlock
     };
     return map;
   }
@@ -55,13 +48,11 @@ class User{
   String toString() {
     return '''
       Sizning id raqamingiz: $id 
-      Sizning ismingiz: $name 
       Sizning taxallusingiz: $username 
-      Sizning yoshingiz: $age 
       Sizning elektron pochtangiz: $email 
       Sizning parolingiz: $password 
       Siz dasturimizdan ro'yxatdan o'tgan sana: $registerData
-      '''.yellow();
+      ''';
   }
 
 }
